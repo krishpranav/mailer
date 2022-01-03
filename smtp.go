e39f813795285cc9d0d889d82aa192693194952f
krishpranav <krisna.pranav@gmail.com>
2022-01-03 18:23:44 +0530

    smtp functions

--- smtp.go
+++ smtp.go
@@ -2,8 +2,13 @@ package mailer
 
 import (
 	"crypto/tls"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
 	"net"
 	"net/textproto"
+	"strings"
 )
 
 type smtpClient struct {
@@ -12,7 +17,6 @@ type smtpClient struct {
 	tls        bool
 	serverName string
 	ext        map[string]string
-
 	a          []string
 	localName  string
 	didHello   bool
@@ -46,8 +50,217 @@ func (c *smtpClient) hello() error {
 	return c.helloError
 }
 
+func (c *smtpClient) hi(localName string) error {
+	if err := validateLine(localName); err != nil {
+		return err
+	}
+	if c.didHello {
+		return errors.New("smtp: Hello called after other methods")
+	}
+	c.localName = localName
+	return c.hello()
+}
+
+func (c *smtpClient) cmd(expectCode int, format string, args ...interface{}) (int, string, error) {
+	id, err := c.text.Cmd(format, args...)
+	if err != nil {
+		return 0, "", err
+	}
+	c.text.StartResponse(id)
+	defer c.text.EndResponse(id)
+	code, msg, err := c.text.ReadResponse(expectCode)
+	return code, msg, err
+}
+
 func (c *smtpClient) helo() error {
 	c.ext = nil
 	_, _, err := c.cmd(250, "HELO %s", c.localName)
 	return err
+}
+
+func (c *smtpClient) ehlo() error {
+	_, msg, err := c.cmd(250, "EHLO %s", c.localName)
+	if err != nil {
+		return err
+	}
+	ext := make(map[string]string)
+	extList := strings.Split(msg, "\n")
+	if len(extList) > 1 {
+		extList = extList[1:]
+		for _, line := range extList {
+			args := strings.SplitN(line, " ", 2)
+			if len(args) > 1 {
+				ext[args[0]] = args[1]
+			} else {
+				ext[args[0]] = ""
+			}
+		}
+	}
+	if mechs, ok := ext["AUTH"]; ok {
+		c.a = strings.Split(mechs, " ")
+	}
+	c.ext = ext
+	return err
+}
+
+func (c *smtpClient) startTLS(config *tls.Config) error {
+	if err := c.hello(); err != nil {
+		return err
+	}
+	_, _, err := c.cmd(220, "STARTTLS")
+	if err != nil {
+		return err
+	}
+	c.conn = tls.Client(c.conn, config)
+	c.text = textproto.NewConn(c.conn)
+	c.tls = true
+	return c.ehlo()
+}
+
+func (c *smtpClient) authenticate(a auth) error {
+	if err := c.hello(); err != nil {
+		return err
+	}
+	encoding := base64.StdEncoding
+	mech, resp, err := a.start(&serverInfo{c.serverName, c.tls, c.a})
+	if err != nil {
+		c.quit()
+		return err
+	}
+	resp64 := make([]byte, encoding.EncodedLen(len(resp)))
+	encoding.Encode(resp64, resp)
+	code, msg64, err := c.cmd(0, strings.TrimSpace(fmt.Sprintf("AUTH %s %s", mech, resp64)))
+	for err == nil {
+		var msg []byte
+		switch code {
+		case 334:
+			msg, err = encoding.DecodeString(msg64)
+		case 235:
+			msg = []byte(msg64)
+		default:
+			err = &textproto.Error{Code: code, Msg: msg64}
+		}
+		if err == nil {
+			resp, err = a.next(msg, code == 334)
+		}
+		if err != nil {
+			c.cmd(501, "*")
+			c.quit()
+			break
+		}
+		if resp == nil {
+			break
+		}
+		resp64 = make([]byte, encoding.EncodedLen(len(resp)))
+		encoding.Encode(resp64, resp)
+		code, msg64, err = c.cmd(0, string(resp64))
+	}
+	return err
+}
+
+func (c *smtpClient) mail(from string, extArgs ...map[string]string) error {
+	var args []interface{}
+	var extMap map[string]string
+
+	if len(extArgs) > 0 {
+		extMap = extArgs[0]
+	}
+
+	if err := validateLine(from); err != nil {
+		return err
+	}
+	if err := c.hello(); err != nil {
+		return err
+	}
+	cmdStr := "MAIL FROM:<%s>"
+	if c.ext != nil {
+		if _, ok := c.ext["8BITMIME"]; ok {
+			cmdStr += " BODY=8BITMIME"
+		}
+		if _, ok := c.ext["SMTPUTF8"]; ok {
+			cmdStr += " SMTPUTF8"
+		}
+		if _, ok := c.ext["SIZE"]; ok {
+			if extMap["SIZE"] != "" {
+				cmdStr += " SIZE=%s"
+				args = append(args, extMap["SIZE"])
+			}
+		}
+	}
+	args = append([]interface{}{from}, args...)
+	_, _, err := c.cmd(250, cmdStr, args...)
+	return err
+}
+
+func (c *smtpClient) rcpt(to string) error {
+	if err := validateLine(to); err != nil {
+		return err
+	}
+	_, _, err := c.cmd(25, "RCPT TO:<%s>", to)
+	return err
+}
+
+type dataCloser struct {
+	c *smtpClient
+	io.WriteCloser
+}
+
+func (d *dataCloser) Close() error {
+	d.WriteCloser.Close()
+	_, _, err := d.c.text.ReadResponse(250)
+	return err
+}
+
+func (c *smtpClient) data() (io.WriteCloser, error) {
+	_, _, err := c.cmd(354, "DATA")
+	if err != nil {
+		return nil, err
+	}
+	return &dataCloser{c, c.text.DotWriter()}, nil
+}
+
+func (c *smtpClient) extension(ext string) (bool, string) {
+	if err := c.hello(); err != nil {
+		return false, ""
+	}
+	if c.ext == nil {
+		return false, ""
+	}
+	ext = strings.ToUpper(ext)
+	param, ok := c.ext[ext]
+	return ok, param
+}
+
+func (c *smtpClient) reset() error {
+	if err := c.hello(); err != nil {
+		return err
+	}
+	_, _, err := c.cmd(250, "RSET")
+	return err
+}
+
+func (c *smtpClient) noop() error {
+	if err := c.hello(); err != nil {
+		return err
+	}
+	_, _, err := c.cmd(250, "NOOP")
+	return err
+}
+
+func (c *smtpClient) quit() error {
+	if err := c.hello(); err != nil {
+		return err
+	}
+	_, _, err := c.cmd(221, "QUIT")
+	if err != nil {
+		return err
+	}
+	return c.text.Close()
+}
+
+func validateLine(line string) error {
+	if strings.ContainsAny(line, "\n\r") {
+		return errors.New("smtp: A line must not contain CR or LF")
+	}
+	return nil
 }
