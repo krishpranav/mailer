a6b9b9f28531a4c40c51c1c2142c1075a60a6289
krishpranav <krisna.pranav@gmail.com>
2022-01-03 19:10:23 +0530

    header

--- header.go
+++ header.go
@@ -1,18 +1,25 @@
 // headers.go implements "Q" encoding as specified by RFC 2047.
-//Modified from https://github.com/joegrasse/mime
+//Modified from https://github.com/joegrasse/mime to use with Go Simple Mail
+
 package mailer
 
 import (
 	"bufio"
 	"bytes"
+	"fmt"
 	"io"
 	"strings"
+	"unicode/utf8"
 )
 
 type encoder struct {
 	w         *bufio.Writer
 	charset   string
 	usedChars int
+}
+
+func newEncoder(w io.Writer, c string, u int) *encoder {
+	return &encoder{bufio.NewWriter(w), strings.ToUpper(c), u}
 }
 
 func (e *encoder) encode(p []byte) (n int, err error) {
@@ -38,7 +45,6 @@ func (e *encoder) encode(p []byte) (n int, err error) {
 		firstWord := true
 
 		for _, word := range words {
-
 			newWord := ""
 			if !firstWord {
 				newWord += " "
@@ -47,7 +53,6 @@ func (e *encoder) encode(p []byte) (n int, err error) {
 
 			if (e.usedChars+len(lineBuffer)+len(newWord)) > maxLineLength && (lineBuffer != "" || e.usedChars != 0) {
 				output.WriteString(lineBuffer + "\r\n")
-
 				if !firstWord {
 					lineBuffer = ""
 				} else {
@@ -60,13 +65,13 @@ func (e *encoder) encode(p []byte) (n int, err error) {
 			lineBuffer += newWord
 
 			firstWord = false
+
 		}
 
 		output.WriteString(lineBuffer)
 
 	} else {
 		firstLine := true
-
 		if e.usedChars == 0 {
 			maxLineLength = 75
 		}
@@ -78,6 +83,7 @@ func (e *encoder) encode(p []byte) (n int, err error) {
 
 		for i := 0; i < len(p); {
 			encodedChar, runeLength := encode(p, i)
+
 			if len(lineBuffer)+e.usedChars+len(encodedChar) > (maxLineLength - len(wordEnd)) {
 				output.WriteString(lineBuffer + wordEnd + "\r\n")
 				lineBuffer = " " + wordBegin
@@ -87,6 +93,7 @@ func (e *encoder) encode(p []byte) (n int, err error) {
 			lineBuffer += encodedChar
 
 			i = i + runeLength
+
 			if !firstLine {
 				e.usedChars = 0
 				maxLineLength = 76
@@ -103,8 +110,34 @@ func (e *encoder) encode(p []byte) (n int, err error) {
 	return n, nil
 }
 
-func NewEncoder(w io.Writer, c string, u int) *encoder {
-	return &encoder{bufio.NewWriter(w), strings.ToUpper(c), u}
+func encode(text []byte, i int) (encodedString string, runeLength int) {
+	started := false
+
+	for ; i < len(text) && (!utf8.RuneStart(text[i]) || !started); i++ {
+		switch c := text[i]; {
+		case c == ' ':
+			encodedString += "_"
+		case isVchar(c) && c != '=' && c != '?' && c != '_':
+			encodedString += string(c)
+		default:
+			encodedString += fmt.Sprintf("=%02X", c)
+		}
+
+		runeLength++
+
+		started = true
+	}
+
+	return
+}
+
+func secureHeader(text []byte) []byte {
+	secureValue := strings.TrimSpace(string(text))
+	secureValue = strings.Replace(secureValue, "\r", "", -1)
+	secureValue = strings.Replace(secureValue, "\n", "", -1)
+	secureValue = strings.Replace(secureValue, "\t", "", -1)
+
+	return []byte(secureValue)
 }
 
 func isVchar(c byte) bool {
