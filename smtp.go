9f71e68d67e94fe3c46cb7d1bd078a5d6fedf4c8
krishpranav <krisna.pranav@gmail.com>
2022-01-03 18:20:27 +0530

    smtp: [helo, hello email]

--- smtp.go
+++ smtp.go
@@ -30,3 +30,24 @@ func newClient(conn net.Conn, host string) (*smtpClient, error) {
 	_, c.tls = conn.(*tls.Conn)
 	return c, nil
 }
+
+func (c *smtpClient) close() error {
+	return c.text.Close()
+}
+
+func (c *smtpClient) hello() error {
+	if !c.didHello {
+		c.didHello = true
+		err := c.ehlo()
+		if err != nil {
+			c.helloError = c.helo()
+		}
+	}
+	return c.helloError
+}
+
+func (c *smtpClient) helo() error {
+	c.ext = nil
+	_, _, err := c.cmd(250, "HELO %s", c.localName)
+	return err
+}
